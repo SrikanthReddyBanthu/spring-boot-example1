1ea35c40d47fbdd958c4741dc47229a4a8f50d3e
Srikanth Reddy Banthu <35705507+SrikanthReddyBanthu@users.noreply.github.com>
2021-11-01 17:19:11 +0530

    Added encryption and decryption process

--- src/main/java/com/spring/example1/demo/controller/Example1Controller.java
+++ src/main/java/com/spring/example1/demo/controller/Example1Controller.java
@@ -1,18 +1,26 @@
 package com.spring.example1.demo.controller;
 
 
+import com.spring.example1.demo.service.JasyptService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Example1Controller {
 
+
+    @Autowired
+    private JasyptService jasyptService;
+
     @Value("${encrypted.property1}")
     private String property1;
 
     @RequestMapping("/")
     public String index() {
+        jasyptService.test();
         return property1;
     }
 
--- src/main/java/com/spring/example1/demo/service/JasyptService.java
+++ src/main/java/com/spring/example1/demo/service/JasyptService.java
@@ -1,9 +1,31 @@
 package com.spring.example1.demo.service;
 
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JasyptService {
 
+    public void test() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
 
+        config.setPassword("cjss_encryption"); // encryptor's private key
+
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+
+        encryptor.setConfig(config);
+
+        String data = "Welcome to CJSS";
+        String encryptedData = encryptor.encrypt(data);
+        System.out.println("encrypted data:" + encryptedData);
+
+        System.out.println("decrypted data:" + encryptor.decrypt(encryptedData));
+    }
 }
